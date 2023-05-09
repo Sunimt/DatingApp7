42873f5182265b09ebbde11dec4be3a733953dd8
Sunimt <dorysunim@gmail.com>
2023-05-09 17:54:18 -0600

    End of section 2

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,10 @@
+using System.Security.AccessControl;
+
+namespace API.Entities
+{
+    public class AppUser
+    {
+       public int Id { get; set; } 
+       public string UserName { get; set; }
+    }
+}
